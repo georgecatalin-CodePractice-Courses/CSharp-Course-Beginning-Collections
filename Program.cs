c53299426300d95715b4865a707bddd0d79f7dcb
George Calin <george.calin@lacon.de>
2019-09-25 11:23:00 +0300

    Completed Chapter 1 about Arrays.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace BeginningCSharp
     {
         static void Main(string[] args)
         {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wenesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            daysOfWeek[2] = "Wednesday";
+
+            foreach (var item in daysOfWeek)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Which day you want to display?");
+            Console.WriteLine("Monday=1, etc >");
+
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay-1];
+            Console.WriteLine($"That day is {chosenDay}");
+
         }
     }
 }
