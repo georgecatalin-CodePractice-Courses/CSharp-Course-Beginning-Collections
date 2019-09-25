d70b5179b1034cdfe49ecfc83057dad47c2fbe57
George Calin <george.calin@lacon.de>
2019-09-25 14:00:26 +0300

    Completed second chapter.

---
+++ CsvReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginningCSharp
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int numberOfCountries)
+        {
+            //return null;
+
+            Country[] country = new Country[numberOfCountries];
+
+            using (StreamReader sr=new StreamReader(_csvFilePath))
+            {
+                sr.ReadLine();
+
+                for (int i = 0; i < numberOfCountries; i++)
+                {
+                    string csvLine = sr.ReadLine();
+                    country[i] = ReadCountryFromCSVLine(csvLine);
+                }
+            }
+
+
+            return country;
+
+        }
+
+        public Country ReadCountryFromCSVLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(new char[] { ',' });
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population =int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace BeginningCSharp
     {
         static void Main(string[] args)
         {
+            string filePath = @"D:\CodeLab\Cursuri Pluralsight\Beginning Collections with C#\BeginningCSharp\Pop by Largest Final.csv";
+
+            CsvReader csvReader = new CsvReader(filePath);
+
+            Country[] countries = csvReader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{country.Name}: {country.Population} ");
+            }
+            
+
         }
     }
 }
